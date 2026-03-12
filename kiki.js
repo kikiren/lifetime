fcbf2533adc62359013d350ab86b97a20f29e813
Kiki Ren <iamkikiren@gmail.com>
2026-03-12 12:22:41 -0400

    added github action

--- kiki.js
+++ kiki.js
@@ -1,14 +1,10 @@
 import dotenv from 'dotenv';
-dotenv.config({ path: '.env' });
+dotenv.config({ path: '.env.kiki' });
 
 import { book } from "./book.js"
 
-const { OCP_APIM_SUBSCRIPTION_KEY, LIFETIME_USERNAME, LIFETIME_PASSWORD } = process.env;
-const lookUpDayCount = 10
-const programList = [
-    'departmentDescription:Pickleball|Pickleball Open Play: Competitive Mid Intermediate',
-    'departmentDescription:Pickleball|Pickleball Open Play: Intermediate',
-    'departmentDescription:Pickleball|Pickleball Open Play: Low Intermediate',
-]
+const { OCP_APIM_SUBSCRIPTION_KEY, LIFETIME_USERNAME, LIFETIME_PASSWORD, PROGRAM_LIST, LOOKUP_DAY_COUNT } = process.env;
+const lookUpDayCount = parseInt(LOOKUP_DAY_COUNT) || 10
+const programList = PROGRAM_LIST.split("$$$")
 
 book(lookUpDayCount, programList, OCP_APIM_SUBSCRIPTION_KEY, LIFETIME_USERNAME, LIFETIME_PASSWORD);
